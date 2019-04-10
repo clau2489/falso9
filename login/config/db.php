a433fa3c0c60a98c0bc7ea211f55afdb83e848b8
OesteDEV <info@oestedev.com>
2019-04-10 15:40:06 -0300

    ...

--- login/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', 'admin');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'falso-nueve');//Nombre de la base de datos
